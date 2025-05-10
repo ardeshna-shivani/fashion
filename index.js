62279c3879f6aefc29debae6f3fa6349c679d561
ardeshna-shivani <ardeshnashivani@gmail.com>
2025-05-10 13:17:46 +0530

    Fashion website design

---
+++ index.js
@@ -0,0 +1,16 @@
+// let x = "1"; 
+
+// function myFunction() {
+//     let x = 1; 
+// }
+
+// myFunction(); 
+// console.log(x);
+
+// let x = 10;
+// if(x === "10"){
+//   console.log("equal");
+// }
+// else{
+//   console.log("not equal");
+// }
